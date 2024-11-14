2e326aac005379a186cd760d8573ad904bba181d
hector <hector.vinas@institutvidreres.com>
2024-11-14 09:22:17 +0100

    Splash Screen

--- app/src/main/java/com/hector/alumneprogram/DataSource.kt
+++ app/src/main/java/com/hector/alumneprogram/DataSource.kt
@@ -2,10 +2,10 @@ package com.hector.alumneprogram
 
 object DataSource {
     val alumnos = mapOf(
-        "1º" to listOf(Alumno("Ana", 14), Alumno("Luis", 15)),
-        "2º" to listOf(Alumno("Maria", 16), Alumno("Carlos", 17)),
-        "3º" to listOf(Alumno("Jorge", 18), Alumno("Elena", 17)),
-        "4º" to listOf(Alumno("Lucia", 19), Alumno("Roberto", 18))
+        "1rSMX" to listOf(Alumno("Ana", 14), Alumno("Luis", 15), Alumno("Pere", 14), Alumno("Joan", 14),),
+        "2nSMX" to listOf(Alumno("Maria", 16), Alumno("Marc", 14), Alumno("Pedro", 14), Alumno("Carlos", 17)),
+        "1rDAM" to listOf(Alumno("Jorge", 18), Alumno("Carlos", 14), Alumno("Laika", 14), Alumno("Elena", 17)),
+        "2nDAM" to listOf(Alumno("Lucia", 19), Alumno("Marta", 14), Alumno("Calisto", 14), Alumno("Robert", 18))
     )
 
     fun getAlumnosPorCurso(curso: String): List<Alumno> {
